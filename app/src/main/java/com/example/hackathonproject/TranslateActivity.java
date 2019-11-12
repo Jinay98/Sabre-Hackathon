c231a6b651acd28ec70417fdd12a9cc2d8b8dea1
rojagtap <rohanj30@icloud.com>
2019-11-12 22:28:07 +0530

    translate done

--- app/src/main/java/com/example/hackathonproject/TranslateActivity.java
+++ app/src/main/java/com/example/hackathonproject/TranslateActivity.java
@@ -1,6 +1,14 @@
 package com.example.hackathonproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hackathonproject.Translation;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +16,20 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+
 public class TranslateActivity extends AppCompatActivity {
 
     EditText input;
     TextView target;
     Spinner choice;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +38,35 @@ public class TranslateActivity extends AppCompatActivity {
         input = (EditText)findViewById(R.id.input_text);
         target = (TextView)findViewById(R.id.target);
         choice = (Spinner)findViewById(R.id.picker);
+
+        requestQueue = Volley.newRequestQueue(this);
     }
 
-    public void go(View view){
+    public void go(View view) throws UnsupportedEncodingException {
         String text = input.getText().toString();
         String language = choice.getSelectedItem().toString();
+        String url = "http://192.168.29.184:8000/translate/" + text.replaceAll(" ", "%20") + "/" + "mr/";
 
-        target.setText(Translation.translate(text, language));
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        byte[] bytes = response.toString().getBytes(StandardCharsets.UTF_8);
+                        String text = new String(bytes, StandardCharsets.UTF_8);
+                        System.out.println(text);
+                        target.setText(text);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.toString());
+
+                    }
+                });
+        ///target.setText(Translation.translate(text, language));
+        requestQueue.add(jsonObjectRequest);
 
     }
 
--- app/src/main/java/com/example/hackathonproject/Translation.java
+++ app/src/main/java/com/example/hackathonproject/Translation.java
@@ -3,7 +3,7 @@ package com.example.hackathonproject;
 public class Translation {
 
     public static String translate(String text, String target){
-        return "Hello";
+        return "hello";
     }
 
 }
